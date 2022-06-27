5337fd3c6c7ddecad118dc02991bb05e1d115c04
Jae Lo Presti (DN0) <me@jae.fi>
2022-06-27 21:11:19 +0300

    Frontned: initial sync

---
+++ frontend/includes/header.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    require('utils/mediapi.php');
+    $loggedin = false;
+    if(isset($_SESSION['token'])) {
+        $loggedin = true;
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>MediaPI<?php if(isset($pagetitle)) echo " - " . $pagetitle; ?></title>
+
+    <link rel="stylesheet" type="text/css" href="public/main.css">
+</head>
+<body>
+
