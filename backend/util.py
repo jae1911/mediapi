1b145003a7e4480df4a6bc63de191683fc51ad1e
Jae Lo Presti (DN0) <me@jae.fi>
2022-06-27 18:35:25 +0300

    Backend: add register and login utilities to utiBackend: add register and login utilities to utill

--- backend/util.py
+++ backend/util.py
@@ -4,8 +4,10 @@ from pickle import dumps, loads
 from logging import getLogger
 from os import environ
 from flask import request, jsonify
-from jwt import decode
+from jwt import decode, encode
 from functools import wraps
+from uuid import uuid4
+from datetime import datetime, timedelta
 
 from app import db
 
@@ -63,6 +65,40 @@ class Users(db.Model):
 def init_db():
     """Initialize the DB"""
     db.create_all()
+
+
+# Register user
+def create_user(username, password):
+    """Creates the user in the DB"""
+    hashed_password = generate_password_hash(password)
+    public_id = str(uuid4())
+
+    try:
+        new_user = Users(
+            public_id=public_id, name=username, password=hashed_password, admin=False
+        )
+        db.session.add(new_user)
+        db.session.commit()
+    except:
+        return None
+
+    return public_id
+
+
+# Generate token
+def verify_login(username, password):
+    """Verifies a login request and returns a token if ok"""
+    user = Users.query.filter_by(name=username).first()
+
+    if check_password_hash(user.password, password):
+        token = jwt.encode(
+            {"public_id": user.public_id, "exp": datetime.utcnow() + timedelta(days=7)},
+            app.config["SECRET_KEY"],
+            "HS256",
+        )
+        return token
+
+    return None
 
 
 # Request verification
